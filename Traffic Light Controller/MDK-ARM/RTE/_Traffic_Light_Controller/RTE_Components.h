cd2475287bccd4f6e80d3bce41eade0cbae27f81
Ampicyn <khwan2540@hotmail.com>
2019-03-24 19:21:41 +0700

    Add bare metal keil project for STM32F407VG

---
+++ Traffic Light Controller/MDK-ARM/RTE/_Traffic_Light_Controller/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Traffic Light Controller' 
+ * Target:  'Traffic Light Controller' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
