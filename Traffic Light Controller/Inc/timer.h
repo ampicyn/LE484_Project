d3b96ad57210beccb9097772898f9a85e62a5750
Ampicyn <khwan2540@hotmail.com>
2019-03-24 20:09:30 +0700

    Add timer module

---
+++ Traffic Light Controller/Inc/timer.h
@@ -0,0 +1,24 @@
+#ifndef __TIMER_H
+#define __TIMER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx_hal.h"
+
+extern uint32_t  Tick;
+extern uint32_t  Timeout_Value;
+extern int Timeout_Status;	
+
+
+void Timeout_Config(uint32_t value);
+void Delay(uint32_t value);
+void HAL_IncTick(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
